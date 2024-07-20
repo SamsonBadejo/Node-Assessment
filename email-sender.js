72ca7b7ea4d0db94e9212da8c5deacf6a7bfd78a
Samson Badejo <zwitsam8@gmail.com>
2024-07-20 19:58:59 +0100

    first commit

---
+++ email-sender.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'your-email@gmail.com',
+    pass: 'your-email-password'
+  }
+});
+
+let mailOptions = {
+  from: 'your-email@gmail.com',
+  to: 'your-email@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
